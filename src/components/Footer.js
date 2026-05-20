14e9c795e088f5b34e0acbe6daf9f58e83842ea7
parmanand <parmanand@scalebuild.ai>
2026-05-21 02:17:06 +0530

    complete files

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+    <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2044{" "}
+        <Link to="/" className="hover:underline">
+          Cinemate
+        </Link>
+        . All Rights Reserved.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <a href="https://www.instagram.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">Instagram</a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6"> LinkedIn </a>
+        </li>
+        <li>
+          <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Twitter</a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">Youtube</a>
+        </li>
+        <li>
+          <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className="hover:underline">Facebook</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
